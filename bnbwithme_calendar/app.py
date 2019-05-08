e335ddf557cb6f4ae35a33dae0581c3c6d16651f
chris <chris.gresock@gmail.com>
2019-05-08 16:39:05 -0400

    Added basic logging. Added removal feature. Added log to gitignore

--- bnbwithme_calendar/app.py
+++ bnbwithme_calendar/app.py
@@ -1,23 +1,44 @@
 #!/usr/bin/python3
 from db import models
 from helpers.icalendar import pull as ical_pull
-import datetime, time
+import datetime, time, logging
 Session = models.session()
 
+logging.basicConfig(filename='bnbwithme-calendar.log',level=logging.DEBUG)
 s = Session()
+
+def log_change(prepend:str, ical, reservation):
+        return f'{prepend}: (property - {ical.location.name}) (start - {reservation.start}) (end {reservation.end}) (guest - {reservation.guest}) (service {ical.service})'
+
 
 # Only sync 3 at a time.
 icals = s.query(models.Ical).order_by(models.Ical.updated_at).limit(3)
-for ical in icals:
+for ical in icals:       
     reservations = ical_pull.get_all_reservations(ical)
+    date_map = dict()
+    # Check for reservations
     for reservation in reservations:
-
+        date_map[reservation.start] = reservation.end
         search = s.query(models.Reservation).filter_by(ical=ical).filter_by(start=reservation.start).filter_by(end=reservation.end)
         # New listing detected, add ical and add to location
         if search.first() is None:
             reservation.ical = ical
             ical.location.reservations.append(reservation)
-            print("adding")
+            logging.info(log_change("ADDING", ical, reservation))
+    # Reverse check reservations
+    search = s.query(models.Reservation).filter_by(ical=ical).filter(models.Reservation.end >= datetime.datetime.now().date()).all()
+    if reservations.__len__() == 0:
+        search = []
+        logging.warning(f'ICAL Feed Empty: {ical.id} {ical.link} {ical.location.name}')
+    for reservation in search:
+        end_date = date_map.get(reservation.start)
+        match = False
+        if end_date:
+            match = reservation.end == end_date
+        if not match:
+            # Can only get to this point if there was no matching reservation in ical feed
+            logging.warning(log_change("DELETING", ical, reservation))
+            reservation.delete()
     ical.updated_at = datetime.datetime.utcnow()
     s.commit()
     print(ical.updated_at)
