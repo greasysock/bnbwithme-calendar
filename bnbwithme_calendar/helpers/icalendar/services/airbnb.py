9798daf29b21fe74bb991dd3105d2db384d684f6
chris <chris.gresock@gmail.com>
2020-03-22 13:46:47 -0400

    fixed regex. removed unused

--- bnbwithme_calendar/helpers/icalendar/services/airbnb.py
+++ bnbwithme_calendar/helpers/icalendar/services/airbnb.py
@@ -5,9 +5,6 @@ import re
 class AirbnbConnection(Connection):
   _not_available = {'Not available'}
 
-  def __init__(self, ical:models.Ical):
-    super().__init__(ical)
-  
   @classmethod
   def _process_guest(self, raw_reservation, key_value_reservation, tar_reservation:models.Reservation):
     guest_string = key_value_reservation.get('SUMMARY')
@@ -15,7 +12,7 @@ class AirbnbConnection(Connection):
       tar_reservation.guest = guest_string
       return True
     else:
-      guest_search = re.search('Airbnb \((.*)\)', guest_string, re.IGNORECASE)
+      guest_search = re.search('Airbnb \\((.*)\\)', guest_string, re.IGNORECASE)
       if guest_search and guest_search.group(1) and guest_search.group(1) not in self._not_available:
         tar_reservation.guest = guest_search.group(1)
         return True
--- bnbwithme_calendar/helpers/icalendar/services/vrbo.py
+++ bnbwithme_calendar/helpers/icalendar/services/vrbo.py
@@ -6,9 +6,6 @@ class VrboConnection(Connection):
   _not_available = {'Blocked', 'Reserved'}
   _split_string = '\r\n'
 
-  def __init__(self, ical:models.Ical):
-    super().__init__(ical)
-  
   @classmethod
   def _process_guest(self, raw_reservation, key_value_reservation, tar_reservation:models.Reservation):
     guest_string = key_value_reservation.get('SUMMARY')
@@ -19,7 +16,6 @@ class VrboConnection(Connection):
         tar_reservation.guest = guest_search.group(1)
         return True
       else:
-        if guest_deconstruction[0] in self._not_available:
-          return False
+        if guest_deconstruction[0] in self._not_available: return False
         raise ValueError("Guest not found in array or array is changed!")
     return False
